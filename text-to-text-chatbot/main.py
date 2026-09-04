119d3cf3fff70a934e7f723561a378b70e7f2c4f
Ashisheoran <ashisheoran672004@gmail.com>
2026-09-04 17:17:08 +0530

    adding ui for chatbot

--- text-to-text-chatbot/main.py
+++ text-to-text-chatbot/main.py
@@ -6,26 +6,40 @@ load_dotenv()
 
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
-def txt_2_txt_model(prompt):
+def txt_2_txt_model(prompt,history):
 
     model = init_chat_model("groq:openai/gpt-oss-120b")
 
-    # response = model.invoke("What is Ai in brief?")
-
-    # print(response.content[0]['text'])
 
     from langchain.messages import HumanMessage, SystemMessage
 
 
     messages = [
-        SystemMessage(content="You are Helping Expert Agent who help the user to solve his query. Give response as better as you can and generate only which user want instead of giving long and unnessary response. And one more thing understand user context what he want and give answer as per queastion."),
+        SystemMessage(content="You are Helping Expert Agent named c2c who help the user to solve his query. Give response as better as you can and generate only which user want instead of giving long and unnessary response. And one more thing understand user context what he want and give answer as per queastion."),
         HumanMessage(prompt)
     ]
 
-    for chunk in model.stream(messages):
-        print(chunk.text, end="")
+    response = model.invoke(messages)
+    # print(response.content)
+
+    return response.content
+
+    # for chunk in model.stream(messages):
+    #     print(chunk.text, end="")
 
 
-user_input = input("\nEnter Your Query:")
+# user_input = input("\nEnter Your Query:")
 
-txt_2_txt_model(user_input)
+# txt_2_txt_model(user_input)
+
+
+import gradio as gr
+
+demo = gr.ChatInterface(
+    fn=txt_2_txt_model,
+    title="My AI assistant",
+    textbox=gr.Textbox(
+        placeholder="Type your message here...."
+    )
+)
+demo.launch()
